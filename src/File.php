aa11486fb820c091b906a93b4059b67e74c8055b
Doug Harple <dharple@gmail.com>
2017-03-31 01:09:50 -0400

    Bug fixes, added 3rd leg

--- src/File.php
+++ src/File.php
@@ -31,6 +31,17 @@ class File
 	protected $size = null;
 
 	/**
+	 *
+	 * @var int
+	 */
+	protected $sizeThreshold = 16*1024*1024; // anything bigger than 16 megs runs md5 from the shell
+
+	/**
+	 *
+	 */
+	protected $sleepTime = 100000;
+
+	/**
 	 * cached checksum
 	 *
 	 * @var string
@@ -72,14 +83,23 @@ class File
 			return $this->sum;
 		}
 
-		return $this->sum = md5(file_get_contents($this->getPath()));
+		if ($this->getSize() <= $this->sizeThreshold) {
+			$this->sum = md5(file_get_contents($this->getPath()));
+		} else {
+			$this->sum = exec('md5sum ' . escapeshellarg($this->getPath()) . ' | cut -f 1 -d " "');
+		}
+
+		// sleep briefly; that was expensive
+		usleep($this->sleepTime);
+
+		return $this->sum;
 	}
 
 	/**
 	 *
 	 */
 	public function isUnique($isUnique = null) {
-		if ($isUnique !== null) {
+		if ($isUnique === null) {
 			return $this->isUnique;
 		}
 
