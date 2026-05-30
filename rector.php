a434f5d8c88e7c5f69c1915f7e7c81921f0ba1bc
Doug Harple <dharple@gmail.com>
2026-05-30 05:32:16 -0400

    Bump phpstan and rector to version 2

--- rector.php
+++ rector.php
@@ -3,52 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/phar',
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-        SymfonySetList::SYMFONY_54,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-    ]);
-
-    // skip individual rules
-    $rectorConfig->skip([
-        //
-        // I'm skipping this because I want to make sure it's correct before
-        // proceeding.
-        //
-
-        \Rector\Php80\Rector\FunctionLike\UnionTypesRector::class,
-
-        //
-        // I'm skipping this because it's stupid.
-        //
-
-        \Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector::class,
-
-        //
-        // I'm skipping this because it makes the code less readable.  We're
-        // instead relying on Symfony to do what it says it will do.
-        //
-
-        \Rector\Php71\Rector\FuncCall\CountOnNullRector::class,
-
-        //
-        // I'm skipping this for now while I review why Rector thinks casts are
-        // necessary here.
-        //
-
-        \Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
-
-    ]);
-};
+    ])
+    ->withPhpSets(php82: true)
+    ->withImportNames(
+        importShortClasses: false,
+        removeUnusedImports: true,
+    )
+    ->withPreparedSets(symfonyCodeQuality: true)
+    ->withComposerBased(symfony: true);
